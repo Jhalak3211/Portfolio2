526d88d95c6ddf94a4deda29c963cc7d252bbfc4
Jhalak yadav <yadavjhalak3211@gmail.com>
2025-06-23 15:04:43 +0530

    Home page added

--- MyPortfolio/react-portfolio/src/components/Navbar/Hero/Hero.jsx
+++ MyPortfolio/react-portfolio/src/components/Navbar/Hero/Hero.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { getImageUrl } from "../../../utils";
-//
+import styles from "./Hero.module.css";
 
 export const Hero = () => {
   return (
-  <section>
-    <div>
-        <h1>Hi,I'm Jhalak</h1>
-        <p>a 2nd-year B.Tech student at MNNIT Allahabad, specializing in Electronics Engineering. I'm passionate about combining creativity with technology—currently exploring graphic design and diving into web development 
+  <section className={styles.container}>
+    <div className={styles.content}>
+        <h1 className={styles.tittle}>
+          Hi,I'm Jhalak
+          </h1>
+        <p className={styles.description}>a 2nd-year B.Tech student at MNNIT Allahabad, specializing in Electronics Engineering. I'm passionate about combining creativity with technology—currently exploring graphic design and diving into web development 
 
         </p>
-        <a href="mailto:jhalak.20244086@mnnit.ac.in">Contact Me</a>
+        <a href="mailto:jhalak.20244086@mnnit.ac.in" className={styles.contactBtn}>Contact Me</a>
     </div>
-    <img src= {getImageUrl("hero/heroImage.png")}alt="hero image of me" />
-    
+    <img src= {getImageUrl("hero/heroImage.png")}alt="hero image of me" className={styles.heroImg} />
+    <div className={styles.topBlur}/>
+    <div className={styles.bottomBlur} />
   </section>
   );
 
